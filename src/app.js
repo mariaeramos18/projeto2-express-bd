3f039cf34d514d74514eabaa0456bd5cdc68b4e1
Maria Eduarda Ramos <mariaeramosfacul@gmail.com>
2026-08-11 19:00:06 -0300

    Atualiza app.js - Aula 11/08

--- src/app.js
+++ src/app.js
@@ -5,4 +5,11 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Minha API REST com Express!')
 })
+
+//Rota lista
+app.get('/lista', (req, res) => {
+  resizeTo.send('Lista do SENAC');
+});
+
+
 export default app
